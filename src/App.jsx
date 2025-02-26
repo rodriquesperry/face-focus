9b80752ead04e632f3d6a0eae0d4efd873374332
Rodriques <rodriques.perry@gmail.com>
2025-02-25 18:06:22 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <ul>
+      <li>Header</li>
+      <li>Logo</li>
+      <li>Searchbar</li>
+      <li>HomePage</li>
+      <li>Signup/Signin</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
